000ea63f35ada10c0743939b53b3e3e52dc3fe95
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-13 09:14:07 +0700

    feat<tab navigator>: create tab navigator and change composition Stack Nav

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,32 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Coba from '../pages/Coba/Coba';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Splash from '../pages/Splash/Splash';
 import Home from '../pages/Home';
+import Order from '../pages/Order';
+import Account from '../pages/Account';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+   return (
+      <Tab.Navigator initialRouteName="Splash">
+         <Tab.Screen name="Splash" component={Splash} />
+         <Tab.Screen name="Home" component={Home} />
+         <Tab.Screen name="Order" component={Order} />
+         <Tab.Screen name="Account" component={Account} />
+      </Tab.Navigator>
+   );
+};
 const Router = () => {
    return (
-         <Stack.Navigator>
-            <Stack.Screen name="home" component={Home} />
-            <Stack.Screen name="coba" component={Coba} />
-         </Stack.Navigator>
+      <Stack.Navigator initialRouteName="MainApp">
+         <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
+         <Stack.Screen name="MainApp" component={MainApp} options={{ headerShown: false }} />
+      </Stack.Navigator>
    );
 };
 
