071906d0d757de04c7f3e56301fd2270e5aed430
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-13 09:12:50 +0700

    feat<>: initial create Service page

---
+++ src/pages/Service/index.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Service = () => {
+   return (
+      <View style={styles.container}>
+         <Text>Service</Text>
+      </View>
+   );
+};
+
+export default Service;
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+   },
+});
