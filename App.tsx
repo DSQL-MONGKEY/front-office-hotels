2b9e19e0e65800f6ac44a674212f8710c4716da9
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-13 09:07:24 +0700

    feat<routes>: create initial routes navigation container

---
+++ App.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Router from './src/routes';
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Router/>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
