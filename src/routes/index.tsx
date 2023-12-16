d09293c4160c77b119bec6686700727f6a573399
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-16 14:56:39 +0700

    chore<>: remove blank space

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,27 @@
+/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Splash from '../pages/Splash/Splash';
 import Home from '../pages/Home';
 import Order from '../pages/Order';
 import Account from '../pages/Account';
+import { BottomNavigator } from '../components';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const MainApp = () => {
    return (
-      <Tab.Navigator>
-         <Tab.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+      <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
+         <Tab.Screen 
+            name="Home"
+            component={Home}
+            options={{ headerShown: false}}
+            />
          <Tab.Screen name="Order" component={Order} options={{ headerShown: false }} />
          <Tab.Screen name="Account" component={Account} options={{ headerShown: false }} />
       </Tab.Navigator>
