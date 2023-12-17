783ae43d7d570b8bbc3693a336494a59e458e3c7
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-17 14:39:46 +0700

    chore<>: change name

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,12 @@
 /* eslint-disable prettier/prettier */
 import logo from './logo-splash.png';
 import bgSplash from './bg-splash.png';
+import logoBrand from './logo.png';
+import logoBrandBlue from './logo-blue.png';
 
 export {
    logo,
    bgSplash,
+   logoBrand,
+   logoBrandBlue,
 };
