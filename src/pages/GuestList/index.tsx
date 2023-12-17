bc2968a60e62f314ff07e6f884513ac1cef20a7b
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-17 14:34:12 +0700

    feat<>: initial create

---
+++ src/pages/GuestList/index.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const GuestList = () => {
+   return (
+      <View style={styles.container}>
+         <Text>GuestList</Text>
+      </View>
+   );
+};
+
+export default GuestList;
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+   },
+});
