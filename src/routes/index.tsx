ba5b625c5b2f7c35535eb63292bcfc539efe79df
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-17 14:36:27 +0700

    feat<>: add route tab navigation for GuestList page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,17 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 import Splash from '../pages/Splash/Splash';
 import Home from '../pages/Home';
-import Order from '../pages/Order';
 import Account from '../pages/Account';
 import { BottomNavigator } from '../components';
+import GuestList from '../pages/GuestList';
+import Service from '../pages/Service';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -17,13 +19,24 @@ const Tab = createBottomTabNavigator();
 const MainApp = () => {
    return (
       <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
-         <Tab.Screen 
+         <Tab.Screen
             name="Home"
             component={Home}
             options={{ headerShown: false}}
             />
-         <Tab.Screen name="Order" component={Order} options={{ headerShown: false }} />
-         <Tab.Screen name="Account" component={Account} options={{ headerShown: false }} />
+         <Tab.Screen
+            name="Guest list"
+            component={GuestList}
+            options={{ headerShown: false }} />
+         <Tab.Screen
+            name="Services"
+            component={Service}
+            options={{ headerShown: false }}
+         />
+         <Tab.Screen
+            name="Account"
+            component={Account}
+            options={{ headerShown: false }} />
       </Tab.Navigator>
    );
 };
