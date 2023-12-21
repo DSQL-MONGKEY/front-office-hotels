8aec8943ca85d605bf0e6a4b06f61d0f82be9d65
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-21 23:50:31 +0700

    feat<>:  implement FlatList

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,29 @@
 /* eslint-disable prettier/prettier */
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, StyleSheet, StatusBar, FlatList } from 'react-native';
 import React from 'react';
+import { roomList } from '../../constants/constants';
+import RoomList from '../../components/RoomList';
 
-const Home = ({ navigation }: any) => {
+const Home = () => {
    return (
       <View style={styles.container}>
-         <Text style={styles.text}>Home SCREEM</Text>
-         <Button title="Account" onPress={() => navigation.navigate('Account')} />
+         <FlatList
+            data={roomList}
+            keyExtractor={item => item.roomName}
+            renderItem={({item}) => <RoomList id={item.id} roomName={item.roomName} imageBanner={item.imageBanner} images={item.imageBanner} />}
+            showsVerticalScrollIndicator={false}
+         />
       </View>
    );
- };
+};
+
 
 const styles = StyleSheet.create({
    container:{
       flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
+      padding:20 ,
+      alignContent: 'center',
+      marginTop: StatusBar.currentHeight || 0,
    },
    text:{ color: '#163020' },
 });
