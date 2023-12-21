443a6f25962ef2a5663a9876ea323fb190bb906b
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-21 23:40:10 +0700

    feat<>: add image rom list assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,10 +3,47 @@ import logo from './logo-splash.png';
 import bgSplash from './bg-splash.png';
 import logoBrand from './logo.png';
 import logoBrandBlue from './logo-blue.png';
+import familyRoom from './family-room.png';
+import familyRoom2 from './family-room2.png';
+import familyRoom3 from './family-room3.png';
+import suitesRoom from './suites-room.jpg';
+import suitesRoom2 from './suites-room2.png';
+import suitesRoom3 from './suites-room3.png';
+import deluxeRoom from './deluxe-room.png';
+import deluxeRoom2 from './deluxe-room2.png';
+import deluxeRoom3 from './deluxe-room3.png';
+import compactRoom from './compact-room.jpg';
+import compactRoom2 from './compact-room2.jpg';
+import compactRoom3 from './compact-room3.jpg';
+import zenRoom from './zen-room.jpg';
+import zenRoom2 from './zen-room2.jpg';
+import zenRoom3 from './zen-room3.jpg';
+import traveRoom from './trave-room.jpg';
+import traveRoom2 from './trave-room2.jpg';
+import traveRoom3 from './trave-room3.jpg';
+
 
 export {
    logo,
    bgSplash,
    logoBrand,
    logoBrandBlue,
+   familyRoom,
+   familyRoom2,
+   familyRoom3,
+   suitesRoom,
+   suitesRoom2,
+   suitesRoom3,
+   deluxeRoom,
+   deluxeRoom2,
+   deluxeRoom3,
+   compactRoom,
+   compactRoom2,
+   compactRoom3,
+   zenRoom,
+   zenRoom2,
+   zenRoom3,
+   traveRoom,
+   traveRoom2,
+   traveRoom3,
 };
