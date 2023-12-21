8abb0ae40568215d419da196965067799d553ce9
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-21 23:49:01 +0700

    feat<>: add initial import for RoomList component

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable prettier/prettier */
 import BottomNavigator from './BottomNavigator';
 import TabItem from './TabItem';
+import Card from './RoomList';
 
 export {
    BottomNavigator,
    TabItem,
+   Card,
 };
