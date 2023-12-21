102e82ae7db3e4437f5c1d335e213a88e0b37c15
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-21 23:53:10 +0700

    chore<>: blank

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
-import Splash from '../pages/Splash/Splash';
+import Splash from '../pages/Splash';
 import Home from '../pages/Home';
 import Account from '../pages/Account';
 import { BottomNavigator } from '../components';
