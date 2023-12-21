9284b58cdab7ffa1a31f2682c9b911bb2b08cafd
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-21 23:33:50 +0700

    feat<>: wrap the app with PaperProvider by react-native-paper

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+/* eslint-disable prettier/prettier */
 /**
  * @format
  */
-import 'react-native-gesture-handler';
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { PaperProvider }  from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+const Main = () => {
+   return (
+      <PaperProvider>
+         <App/>
+      </PaperProvider>
+   )
+}
+
+AppRegistry.registerComponent(appName, () => Main);
