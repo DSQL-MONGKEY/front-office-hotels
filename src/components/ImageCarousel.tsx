28d1e0e46a77a0b7ccafff1102b99d5e4f5b86cd
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-12-24 22:55:50 +0700

    initial

---
+++ src/components/ImageCarousel.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const ImageCarousel = ({ images }) => {
+   return (
+      <View>
+         <Text>ImageCarousel</Text>
+      </View>
+   );
+};
+
+export default ImageCarousel;
+
+const styles = StyleSheet.create({});
