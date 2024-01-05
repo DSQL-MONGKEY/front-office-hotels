114271581d42e2eccab0afd675766ebcfe431c18
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2024-01-05 22:11:48 +0700

    none

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,42 +10,120 @@ const roomList = [
       id: 1,
       roomName: 'Family Room',
       imageBanner: familyRoom,
-      images: [familyRoom, familyRoom2, familyRoom3],
+      images: [
+         {
+            id: 1,
+            image: familyRoom,
+         },
+         {
+            id: 2,
+            image: familyRoom2,
+         },
+         {
+            id: 3,
+            image: familyRoom3,
+         },
+      ],
       qtyPerson: 15,
    },
    {
       id: 2,
       roomName: 'Suites Room',
       imageBanner: suitesRoom,
-      images: [suitesRoom, suitesRoom2, suitesRoom3],
+      images: [
+         {
+            id: 1,
+            image: suitesRoom,
+         },
+         {
+            id: 2,
+            image: suitesRoom2,
+         },
+         {
+            id: 3,
+            image: suitesRoom3,
+         },
+      ],
       qtyPerson: 5,
    },
    {
       id: 3,
       roomName: 'Deluxe Room',
       imageBanner: deluxeRoom,
-      images: [deluxeRoom, deluxeRoom2, deluxeRoom3],
+      images: [
+         {
+            id: 1,
+            image: deluxeRoom,
+         },
+         {
+            id: 2,
+            image: deluxeRoom2,
+         },
+         {
+            id: 1,
+            image: deluxeRoom3,
+         },
+      ],
       qtyPerson: 10,
    },
    {
       id: 4,
       roomName: 'Compact Room',
       imageBanner: compactRoom,
-      images: [compactRoom, compactRoom2, compactRoom3],
+      images: [
+         {
+            id: 1,
+            image: compactRoom,
+         },
+         {
+            id: 2,
+            image: compactRoom2,
+         },
+         {
+            id: 1,
+            image: compactRoom3,
+         },
+      ],
       qtyPerson: 4,
    },
    {
       id: 5,
       roomName: 'Zen Room',
       imageBanner: zenRoom,
-      images: [zenRoom, zenRoom2, zenRoom3],
+      images: [
+         {
+            id: 1,
+            image: zenRoom,
+         },
+         {
+            id: 2,
+            image: zenRoom2,
+         },
+         {
+            id: 3,
+            image: zenRoom3,
+         },
+      ],
       qtyPerson: 4,
    },
    {
       id: 6,
       roomName: 'Trave Room',
       imageBanner: traveRoom,
-      images: [traveRoom, traveRoom2, traveRoom3],
+      images: [
+         {
+            id: 1,
+            image: traveRoom,
+         },
+         {
+            id: 2,
+            image: traveRoom2,
+         },
+         {
+            id: 3,
+            image: traveRoom3,
+         },
+      ],
       qtyPerson: 2,
    },
 ];
