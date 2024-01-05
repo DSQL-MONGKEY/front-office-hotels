c0148bcccfe6f1c9155d9265f54fb463559aced2
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2024-01-05 22:11:39 +0700

    feat<>: update the render of component

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,15 +1,29 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 
-const ImageCarousel = ({ images }) => {
+const ImageCarousel = ({ images }: any) => {
    return (
       <View>
-         <Text>ImageCarousel</Text>
+         <Image source={images.image} style={styles.images} />
       </View>
    );
 };
 
 export default ImageCarousel;
 
-const styles = StyleSheet.create({});
+const screenWidth = Dimensions.get('window').width;
+const styles = StyleSheet.create({
+   images: {
+      width: screenWidth,
+      height: 200,
+   },
+   dot: {
+      backgroundColor: 'red',
+      height: 10,
+      width: 10,
+      borderRadius: 5,
+   },
+});
+
+
