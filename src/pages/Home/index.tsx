a6c08efd98130a427c51be0e27ee901136ea9cdb
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2024-01-14 07:01:11 +0700

    fix<>: fix the issues un bracket render item

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable prettier/prettier */
-import { View, StyleSheet, StatusBar, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import React from 'react';
 import { roomList } from '../../constants/constants';
 import RoomList from '../../components/RoomList';
-import Header from '../../components/Header';
 
 const Home = ({ navigation }: any) => {
    return (
       <View style={styles.container}>
-         <Header/>
          <FlatList
             data={roomList}
             keyExtractor={item => item.roomName}
-            renderItem={({item}) => <RoomList id={item.id} roomName={item.roomName} imageBanner={item.imageBanner} images={item.images} qtyPerson={item.qtyPerson} navigation={navigation} />}
+            renderItem={({item}) => (
+               <RoomList id={item.id} roomName={item.roomName} imageBanner={item.imageBanner} images={item.images} facility={item.facility} qtyPerson={item.qtyPerson} price={item.price} navigation={navigation} />
+            )}
             showsVerticalScrollIndicator={false}
          />
       </View>
@@ -25,10 +25,6 @@ const styles = StyleSheet.create({
       flex: 1,
       alignContent: 'center',
       paddingHorizontal: 20,
-      paddingTop: StatusBar.currentHeight || 0,
-      backgroundColor: '#89ADE2',
-   },
-   flatList: {
    },
 });
 
