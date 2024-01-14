c1ea541dfb265f953a9566212fa7f088f40b3f57
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2024-01-14 07:03:18 +0700

    blank

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,23 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Header = () => {
   return (
       <View>
-         <Text>Header</Text>
+         <Button>
+            <Ionicons name="arrow-back-circle-outline" style={styles.backButton} /> Back
+         </Button>
       </View>
    );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+   backButton: {
+      fontSize: 30,
+   },
+});
