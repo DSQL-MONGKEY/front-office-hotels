2a99536ea05779aeba68189d7e4736ac2eafa1b3
Dimpfe <dimasprasetyo7696@gmail.com>
2024-01-18 20:20:05 +0700

    feat<>: add import rn gesture handler

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import Router from './src/routes';
 
