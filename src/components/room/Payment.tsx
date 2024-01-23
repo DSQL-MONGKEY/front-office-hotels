a00e6fabaaa22b8811e5ae41310fa9e6e50c080f
Dimpfe <dimasprasetyo7696@gmail.com>
2024-01-23 17:15:44 +0700

    intial create

---
+++ src/components/room/Payment.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Payment = () => {
+   return (
+      <View>
+         <Text>Payment</Text>
+      </View>
+   );
+};
+
+export default Payment;
+
+const styles = StyleSheet.create({
+
+});
