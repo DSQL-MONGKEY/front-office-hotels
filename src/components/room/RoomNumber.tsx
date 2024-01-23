b0008b8d91eb949f28749129b24c88b0614d911c
Dimpfe <dimasprasetyo7696@gmail.com>
2024-01-23 17:16:48 +0700

    feat<>: create seperate component RenderItem

--- src/components/room/RoomNumber.tsx
+++ src/components/room/RoomNumber.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import React from 'react';
 import { availableRoomId } from '../../constants/constants';
 import { FlatList } from 'react-native-gesture-handler';
-import { Button } from 'react-native-paper';
-import { useGuestDataStore } from '../../state';
+import RoomItem from './RoomItem';
+
 
 const RoomNumber = () => {
    return (
          <FlatList
             keyExtractor={item => item.id}
             data={availableRoomId}
-            renderItem={({item}):any => <RenderItem item={item} />}
+            renderItem={({item}):any => <RoomItem id={item.id} status={item.status} />}
             horizontal
             style={styles.container}
          />
@@ -19,38 +19,7 @@ const RoomNumber = () => {
 };
 
 
-const RenderItem = ({ item }: any) => {
-   const [selectedRoom, setSelectedRoom] = useState('');
-   const [active, setActive] = useState(false);
-   const updateRoomNumber = useGuestDataStore((state) => state.roomNumber);
-   console.log(updateRoomNumber);
 
-   const handelSelect = () => {
-      setSelectedRoom(item.id);
-      setActive(true);
-      item.status = active;
-      if (active) {
-         setSelectedRoom('');
-         setActive(false);
-         item.status = active;
-      }
-   };
-   return (
-      <View style={styles.itemContainer}>
-         <Button
-            style={
-            [
-               styles.itemContainer,
-               {backgroundColor: item.status ? '#86A7FC' : '#FF2E63'},
-               {backgroundColor: active ? 'black' : '#86A7FC'},
-            ]}
-            onPress={handelSelect}
-         >
-         <Text style={[styles.text]}>{item.id}</Text>
-         </Button>
-      </View>
-   );
-};
 
 export default RoomNumber;
 
@@ -60,18 +29,16 @@ const styles = StyleSheet.create({
       backgroundColor: 'white',
       width: '100%',
       padding: 5,
-      borderRadius: 10,
-      borderWidth:1,
-      borderColor: '#7D5A50',
-   },
-   itemContainer: {
-      flex: 1,
-      paddingTop: 5,
-      margin: 5,
-      borderRadius: 10,
+      textAlign: 'center',
+      borderRadius: 5,
    },
    text: {
+      margin: 4,
+      borderRadius: 5,
       color: 'white',
       fontSize: 18,
+      padding: 9,
+      width: 40,
+      height: 45,
    },
 });
