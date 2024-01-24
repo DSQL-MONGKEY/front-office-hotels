2593ec0d9f34b45a68a30f48879095d0bdae973e
Dimpfe <dimasprasetyo7696@gmail.com>
2024-01-24 23:16:18 +0700

    feat<firebase, state hooks, BottomSheet>: add props value for invoice component adn add state hooks and event handlePress

--- src/components/room/ModalBottom.tsx
+++ src/components/room/ModalBottom.tsx
@@ -1,42 +1,103 @@
 /* eslint-disable prettier/prettier */
-import React, { useMemo, useRef } from 'react';
-import BottomSheet from '@gorhom/bottom-sheet';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import React, { useCallback, useMemo, useRef } from 'react';
+import BottomSheet, { BottomSheetBackdrop, BottomSheetModalProvider, BottomSheetScrollView } from '@gorhom/bottom-sheet';
+import { StyleSheet, View } from 'react-native';
 import Invoice from './Invoice';
 import { Button } from 'react-native-paper';
 import { Text } from 'react-native';
 import InputData from './InputData';
-import { useGuestDataStore } from '../../state';
+import { useGlobalState, useGuestDataStore } from '../../state';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { FIREBASE_APP, FIREBASE_DB } from '../../../FirebaseConfig';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ModalBottom = () => {
-   const snapPoints = useMemo(() => ['25%', '50%', '75%', '100%'], []);
+   const snapPoints = useMemo(() => ['50%', '90%'], []);
    const bottomSheetRef = useRef(null);
+   const renderBackdrop = useCallback((props: any) => <BottomSheetBackdrop appearsOnIndex={0} disappearsOnIndex={-1} {...props} />, []);
 
+   const guestName = useGuestDataStore((state) => state.guestName);
+   const email = useGuestDataStore((state) => state.email);
+   const phoneNum = useGuestDataStore((state) => state.phoneNumber);
+   const address = useGuestDataStore((state) => state.address);
+   const roomType = useGuestDataStore((state) => state.roomType);
+   const roomNumber = useGuestDataStore((state) => state.roomNumber);
+   const deposit = useGuestDataStore((state) => state.deposit);
+   const addServices = useGuestDataStore((state) => state.addServices);
+   const date = useGuestDataStore((state) => state.date);
+   const roomPrice = useGuestDataStore((state) => state.roomPrice);
    const cashAmount = useGuestDataStore((state) => state.cashAmount);
+
    const updateCashAmount = useGuestDataStore((state) => state.updateCashAmount);
+   const updateIsConfirm = useGlobalState((state) => state.updateIsConfirm);
+
+
+   const testdoc = async() => {
+      await addDoc(collection(FIREBASE_DB, 'guests-data'), {
+         guestName: guestName,
+         email: email,
+         phoneNumber: phoneNum,
+         address: address,
+         roomType: roomType,
+         roomNumber: roomNumber,
+         addServices: addServices,
+         dateCheckin: serverTimestamp(),
+         dateCheckout: date,
+         deposit: deposit,
+         roomPrice: roomPrice,
+      });
+   };
+   const handlePress = () => {
+      updateIsConfirm(false);
+   };
+   const handleSheetChanges = useCallback((index: number) => {
+      console.log('handleSheetChanges', index);
+   }, []);
 
    return (
-      <BottomSheet
+      <BottomSheetModalProvider>
+         <BottomSheet
          ref={bottomSheetRef}
          snapPoints={snapPoints}
-
-      >
-         <ScrollView>
-            <View style={styles.contentContainer}>
-               <Invoice />
-               <InputData
-                  type={'cash'}
-                  label={'Cash'}
-                  placeholder={'Enter amount'}
-                  value={cashAmount}
-                  onChangeEvent={updateCashAmount}
-               />
-               <Button contentStyle={styles.button} onPress={() => {}}>
-                     <Text style={styles.buttonText}>Confirm</Text>
-               </Button>
+         enableContentPanningGesture={false}
+         handleIndicatorStyle={styles.indicator}
+         backdropComponent={renderBackdrop}
+         onChange={handleSheetChanges}
+         >
+         <BottomSheetScrollView>
+            <View style={styles.iconContainer}>
+               <MaterialCommunityIcons
+                  name={'close-octagon-outline'} style={styles.closeIcon} onPress={handlePress} />
             </View>
-         </ScrollView>
+            <View style={styles.contentContainer}>
+                  <Invoice
+                     guestName={guestName}
+                     email={email}
+                     phoneNum={phoneNum}
+                     address={address}
+                     roomType={roomType}
+                     roomNumber={roomNumber}
+                     deposit={deposit}
+                     addServices={addServices}
+                     date={date}
+                     roomPrice={roomPrice}
+                  />
+                  <InputData
+                     type={'cash'}
+                     label={'Cash'}
+                     placeholder={'Enter amount'}
+                     onChangeEvent={updateCashAmount}
+                  />
+                  <Button
+                     contentStyle={styles.button}
+                     onPress={() => testdoc()}
+                  >
+                     <Text style={styles.buttonText}>Confirm</Text>
+                  </Button>
+               </View>
+         </BottomSheetScrollView>
       </BottomSheet>
+      </BottomSheetModalProvider>
    );
 };
 
@@ -67,5 +128,19 @@ const styles = StyleSheet.create({
       padding: 50,
       fontFamily: 'Quicksand-Bold',
       color: 'white',
+   },
+   indicator: {
+      borderRadius: 100,
+      paddingHorizontal: 10,
+   },
+   iconContainer: {
+      flex: 1,
+      width: '100%',
+      alignItems: 'flex-end',
+      paddingRight: 20,
+   },
+   closeIcon:{
+      fontSize: 30,
+      color: '#B6C4B6',
    },
 });
