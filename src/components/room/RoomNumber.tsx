6db8386f2cfb7ffe798c6d0f29f31214de4ebca7
Dimpfe <dimasprasetyo7696@gmail.com>
2024-01-24 23:20:59 +0700

    fix, feat<>: fix keyExtractor for FlatList and add RoomItem component

--- src/components/room/RoomNumber.tsx
+++ src/components/room/RoomNumber.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 import { availableRoomId } from '../../constants/constants';
 import { FlatList } from 'react-native-gesture-handler';
-import RoomItem from './RoomItem';
+import { useGuestDataStore } from '../../state';
 
 
 const RoomNumber = () => {
    return (
          <FlatList
-            keyExtractor={item => item.id}
+            keyExtractor={(item, index) => String(index)}
             data={availableRoomId}
             renderItem={({item}):any => <RoomItem id={item.id} status={item.status} />}
             horizontal
@@ -18,6 +18,38 @@ const RoomNumber = () => {
    );
 };
 
+interface RoomItemProps {
+   id: number,
+   status: boolean,
+}
+
+const RoomItem = ({ id, status }: RoomItemProps) => {
+   const [active, setActive] = useState(false);
+   const updateRoomNumber = useGuestDataStore((state) => state.updateRoomNumber);
+
+   const handleSelect = () => {
+      setActive(true);
+      updateRoomNumber(id);
+      if (active) {
+         setActive(false);
+         updateRoomNumber(0);
+      }
+   };
+   return (
+      <View style={styles.container}>
+         <Text
+            key={id}
+            style={[styles.text,
+               {backgroundColor: status ? '#86A7FC' : '#FF2E63'},
+               {backgroundColor: active ? 'black' : '#86A7FC'},
+            ]}
+            onPress={handleSelect}
+         >
+            {id}
+         </Text>
+      </View>
+   );
+};
 
 
 
@@ -25,6 +57,14 @@ export default RoomNumber;
 
 const styles = StyleSheet.create({
    container: {
+      flex: 1,
+      backgroundColor: 'white',
+      width: '100%',
+      padding: 5,
+      textAlign: 'center',
+      borderRadius: 5,
+   },
+   InnerContainer: {
       flex: 1,
       backgroundColor: 'white',
       width: '100%',
