eb92c443947b12814ef944df202e25628558deec
Dimpfe <dimasprasetyo7696@gmail.com>
2024-01-27 07:39:39 +0700

    feat<>: remove firebase analytics

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,7 +1,6 @@
 /* eslint-disable prettier/prettier */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,6 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const ANALYTICS = getAnalytics(FIREBASE_APP);
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
 
