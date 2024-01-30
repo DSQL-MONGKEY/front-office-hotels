3f8dbb4a5130b4f21433bd320b96f3689657c4e7
Dimpfe <dimasprasetyo7696@gmail.com>
2024-01-30 17:40:27 +0700

    feat<>: add BottomSheetMOdalProvider to wrap the intire app

--- App.tsx
+++ App.tsx
@@ -3,15 +3,24 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import Router from './src/routes';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Router/>
-    </NavigationContainer>
+    // eslint-disable-next-line react-native/no-inline-styles
+    <GestureHandlerRootView style={{ flex: 1 }} >
+      <BottomSheetModalProvider>
+        <NavigationContainer>
+          <Router/>
+        </NavigationContainer>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 };
+
+
 
 export default App;
 
