e8559c9b7f6fbf3daab5dee89286d73e50e40a4d
Dimpfe <dimasprasetyo7696@gmail.com>
2024-01-30 17:58:54 +0700

    feat<>: create conditional rendering on FlatList and add bottomSheetRef for BottomSheetModal

--- src/pages/GuestList/index.tsx
+++ src/pages/GuestList/index.tsx
@@ -1,20 +1,24 @@
 /* eslint-disable prettier/prettier */
 import { collection, onSnapshot } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, Text, View, FlatList, StatusBar, Image, Button } from 'react-native';
 import { FIREBASE_DB } from '../../../FirebaseConfig';
-import { Divider } from 'react-native-paper';
+import SkeletonList from '../../components/SkeletonList';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { BottomSheetModal, TouchableOpacity } from '@gorhom/bottom-sheet';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { empty } from '../../assets';
+import CustomBottomSheetModal from '../../components/CustomBottomSheetModal';
 
 const GuestList = () => {
    const [guests, setGuests] = useState<any>([]);
-   const [isLoading, setIsLoading] = useState(false);
+   const bottomSheetRef = useRef<BottomSheetModal>(null);
 
    useEffect(() => {
-      setIsLoading(true);
       const guestListRef = collection(FIREBASE_DB, 'guests-data');
       const subscriber = onSnapshot(guestListRef, {
          next: (snapshot) => {
-            const guest: any[] = [];
+            const guest: object[] = [];
             snapshot.docs.forEach((doc) => {
                guest.push({
                   id: doc.id,
@@ -24,7 +28,6 @@ const GuestList = () => {
             setGuests(guest);
          },
       });
-      setIsLoading(false);
       return () => subscriber();
    }, []);
 
@@ -32,63 +35,104 @@ const GuestList = () => {
 
    const renderItem = ({ item }: any) => {
       return (
-         <>
+         <TouchableOpacity>
             {!item.done &&
                <View key={item.id} style={styles.cardContainer}>
                   <View style={styles.roomType}>
                      <Text style={styles.roomText}>{item.roomType}</Text>
                      <Text style={styles.roomText}>{item.roomNumber}</Text>
                   </View>
-                  <View style={styles.roomInfo}>
-                     <Text style={[styles.text, styles.guestName]}>{item.guestName}</Text>
-                     <Text style={styles.text}>
-                        {item.dateCheckin} - {item.dateCheckout}
-                     </Text>
+                  <View style={styles.roomInfoContainer}>
+                     <View style={styles.innerContainer}>
+                        <Ionicons
+                           name={'person-circle-outline'}
+                           style={styles.icon}
+                        />
+                        <Text style={[styles.text, styles.guestName]}>
+                           {item.guestName}
+                        </Text>
+                     </View>
+                     <View style={styles.innerContainer}>
+                        <Ionicons name={'timer'} style={styles.timeIcon} />
+                        <Text style={styles.text}>
+                           {item.dateCheckin} - {item.dateCheckout}
+                        </Text>
+                     </View>
                   </View>
                </View>
             }
-         </>
+         </TouchableOpacity>
       );
    };
    const renderDoneItem = ({item}: any) => {
       return (
-         <>
+         <TouchableOpacity>
             {item.done &&
                <View key={item.id} style={styles.cardContainer}>
-                  <View style={styles.roomType}>
+                  <View style={[styles.roomType, { backgroundColor: item.done && '#00AA13' }]}>
                      <Text style={styles.roomText}>{item.roomType}</Text>
                      <Text style={styles.roomText}>{item.roomNumber}</Text>
                   </View>
-                  <View style={styles.roomInfo}>
-                     <Text style={[styles.text, styles.guestName]}>{item.guestName}</Text>
-                     <Text style={styles.text}>
-                        {item.dateCheckin} - {item.dateCheckout}
-                     </Text>
+                  <View style={styles.roomInfoContainer}>
+                     <View style={styles.innerContainer}>
+                        <Ionicons
+                           name={'person-circle-outline'}
+                           style={styles.icon}
+                        />
+                        <Text style={[styles.text, styles.guestName]}>
+                           {item.guestName}
+                        </Text>
+                     </View>
+                     <View style={styles.innerContainer}>
+                        <Ionicons name={'timer'} style={styles.timeIcon} />
+                        <Text style={styles.text}>
+                           {item.dateCheckin} - {item.dateCheckout}
+                        </Text>
+                     </View>
                   </View>
                </View>
             }
-         </>
+         </TouchableOpacity>
       );
    };
 
+   // const handlePresentModal = () => sbottomSheetRef.current?.present();
+
    return (
-      <View style={styles.container}>
-         {guests.length > 0 &&
-            <FlatList
-               data={guests}
-               renderItem={renderItem}
-               keyExtractor={(item) => item.id}
-            />
+      <SafeAreaView style={styles.container}>
+         {guests.length < 0 ?
+            (
+               <View style={styles.emptyContainer}>
+               <Image source={empty} style={styles.emptyImage} />
+               <Text style={styles.emptyText}>
+                  No Guests Yet
+               </Text>
+            </View>
+            ) : (
+               <>
+               <Text style={styles.headText}>Active Guest</Text>
+               <FlatList
+                  data={guests}
+                  renderItem={renderItem}
+                  style={styles.flatList}
+                  keyExtractor={(item) => item.id}
+                  showsVerticalScrollIndicator={false}
+                  ListEmptyComponent={<SkeletonList/>}
+               />
+               <Text style={styles.headText}>Inactive Guest</Text>
+               <FlatList
+                  data={guests}
+                  renderItem={renderDoneItem}
+                  style={styles.flatList}
+                  keyExtractor={(item) => item.id}
+                  showsVerticalScrollIndicator={false}
+                  ListEmptyComponent={<SkeletonList/>}
+               />
+            </>
+            )
          }
-         <Divider/>
-         {guests.length > 0 &&
-            <FlatList
-               data={guests}
-               renderItem={renderDoneItem}
-               keyExtractor={(item) => item.id}
-            />
-         }
-      </View>
+         <CustomBottomSheetModal ref={bottomSheetRef} />
+      </SafeAreaView>
    );
 };
 
@@ -96,15 +140,25 @@ export default GuestList;
 
 const styles = StyleSheet.create({
    container: {
-      flex: 1,
-      padding: 20,
-      gap: 10,
+      flex: 2,
+      paddingHorizontal: 16,
+      height: '100%',
+      marginTop: StatusBar.currentHeight,
+      marginBottom: 10,
+      gap: 5,
    },
    cardContainer: {
+      backgroundColor: '#EAEAEA',
+      borderRadius: 10,
+      marginBottom: 20,
+   },
+   flatList: {
       backgroundColor: 'white',
+      paddingHorizontal: 10,
+      paddingVertical: 10,
+      height: '50%',
       borderRadius: 10,
       elevation: 4,
-      marginVertical: 8,
    },
    roomType: {
       flexDirection: 'row',
@@ -115,19 +169,52 @@ const styles = StyleSheet.create({
       borderTopRightRadius: 10,
       borderBottomLeftRadius: 20,
    },
-   roomInfo: {
+   roomInfoContainer: {
       margin: 4,
+   },
+   innerContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
    },
    roomText: {
       color: 'white',
       fontFamily: 'Quicksand-Bold',
       fontSize: 15,
    },
+   headText: {
+      color: 'black',
+      fontFamily: 'Maison',
+      fontSize: 24,
+      textAlign: 'center',
+   },
    text: {
       color: 'black',
-      fontFamily: 'Quicksand-Bold',
+      fontFamily: 'Maison',
    },
    guestName: {
       fontSize: 20,
+   },
+   icon: {
+      color: 'white',
+      fontSize: 35,
+   },
+   timeIcon: {
+      fontSize: 20,
+      color: 'white',
+   },
+   emptyContainer: {
+      width: '100%',
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+   },
+   emptyImage: {
+      width: 140,
+      height: 90,
+   },
+   emptyText: {
+      fontSize: 16,
+      fontFamily: 'Maison',
+      color: '#86A7FC',
    },
 });
