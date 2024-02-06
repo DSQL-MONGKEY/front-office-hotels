382d072030b5f08e9064a6ea899fc5b11f0ee134
Dimpfe <dimasprasetyo7696@gmail.com>
2024-02-06 09:45:26 +0700

    chore<>: adjust widht the svg

--- src/components/SkeletonList.tsx
+++ src/components/SkeletonList.tsx
@@ -9,29 +9,33 @@ const SkeletonList = () => {
          width={476}
          height={300}
          viewBox="0 0 476 300"
-         backgroundColor="#f3f3f3"
+         backgroundColor="#ffffff"
          foregroundColor="#ecebeb"
       >
-         <Rect x="50" y="20" rx="3" ry="3" width="88" height="6" />
-         <Rect x="50" y="38" rx="3" ry="3" width="52" height="6" />
-         <Rect x="-1" y="63" rx="3" ry="3" width="275" height="6" />
-         <Rect x="0" y="75" rx="3" ry="3" width="232" height="6" />
-         <Rect x="0" y="88" rx="3" ry="3" width="178" height="6" />
-         <Circle cx="22" cy="32" r="20" />
-
-         <Rect x="52" y="113" rx="3" ry="3" width="88" height="6" />
-         <Rect x="52" y="131" rx="3" ry="3" width="52" height="6" />
-         <Rect x="1" y="156" rx="3" ry="3" width="275" height="6" />
-         <Rect x="2" y="168" rx="3" ry="3" width="232" height="6" />
-         <Rect x="2" y="181" rx="3" ry="3" width="178" height="6" />
-         <Circle cx="24" cy="125" r="20" />
-
-         <Rect x="53" y="203" rx="3" ry="3" width="88" height="6" />
-         <Rect x="53" y="221" rx="3" ry="3" width="52" height="6" />
-         <Rect x="2" y="246" rx="3" ry="3" width="275" height="6" />
-         <Rect x="3" y="258" rx="3" ry="3" width="232" height="6" />
-         <Rect x="3" y="271" rx="3" ry="3" width="178" height="6" />
-         <Circle cx="25" cy="215" r="20" />
+      <Rect x="49" y="19" rx="3" ry="3" width="88" height="6" />
+      <Rect x="49" y="38" rx="3" ry="3" width="52" height="6" />
+      <Rect x="1" y="68" rx="3" ry="3" width="410" height="6" />
+      <Rect x="1" y="84" rx="3" ry="3" width="380" height="6" />
+      <Rect x="1" y="100" rx="3" ry="3" width="178" height="6" />
+      <Circle cx="21" cy="32" r="20" />
+      <Rect x="50" y="123" rx="3" ry="3" width="88" height="6" />
+      <Rect x="50" y="142" rx="3" ry="3" width="52" height="6" />
+      <Rect x="2" y="172" rx="3" ry="3" width="410" height="6" />
+      <Rect x="2" y="188" rx="3" ry="3" width="380" height="6" />
+      <Rect x="2" y="204" rx="3" ry="3" width="178" height="6" />
+      <Circle cx="22" cy="136" r="20" />
+      <Rect x="49" y="226" rx="3" ry="3" width="88" height="6" />
+      <Rect x="49" y="245" rx="3" ry="3" width="52" height="6" />
+      <Rect x="1" y="275" rx="3" ry="3" width="410" height="6" />
+      <Rect x="1" y="291" rx="3" ry="3" width="380" height="6" />
+      <Rect x="1" y="307" rx="3" ry="3" width="178" height="6" />
+      <Circle cx="21" cy="239" r="20" />
+      <Rect x="52" y="329" rx="3" ry="3" width="88" height="6" />
+      <Rect x="52" y="348" rx="3" ry="3" width="52" height="6" />
+      <Rect x="4" y="378" rx="3" ry="3" width="410" height="6" />
+      <Rect x="4" y="394" rx="3" ry="3" width="380" height="6" />
+      <Rect x="4" y="410" rx="3" ry="3" width="178" height="6" />
+      <Circle cx="24" cy="342" r="20" />
       </ContentLoader>
    );
 };
